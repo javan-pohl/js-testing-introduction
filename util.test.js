7617f4012161ebb9799d4ec619128eaee22567d2
LordBigglesworth <javanpohl@gmail.com>
2021-06-16 13:08:07 -0600

    reconfigured the addUser function to be more modudular and implmented an integration test to test its functionality

--- util.test.js
+++ util.test.js
@@ -1,8 +1,10 @@
-const { generateText } = require("./util");
+const { generateText, checkAndGenerate } = require("./util");
+
+const javan = "javan (38 years old)"
 
 test("should output name and age", () => {
   const text = generateText("javan", 38);
-  expect(text).toBe("javan (38 years old)");
+  expect(text).toBe(javan);
   const text2 = generateText("anna", 298);
   expect(text2).toBe("anna (298 years old)");
 });
@@ -11,3 +13,8 @@ test("should output data-less text", () => {
   const text = generateText("", null);
   expect(text).toBe(" (null years old)");
 });
+
+test('should generate a valid text output', () => {
+	const text = checkAndGenerate('javan', 38)
+	expect(text).toBe(javan)
+})
