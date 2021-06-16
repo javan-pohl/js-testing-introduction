3550dbd6304e5e83be45d5b547a6e47854b4d23d
LordBigglesworth <javanpohl@gmail.com>
2021-06-16 15:03:53 -0600

    implemented e2e testing suite with puppeteer

--- util.test.js
+++ util.test.js
@@ -1,6 +1,8 @@
+const puppeteer = require("puppeteer");
 const { generateText, checkAndGenerate } = require("./util");
 
-const javan = "javan (38 years old)"
+const javan = "javan (38 years old)";
+const Anna = "Anna (29 years old)";
 
 test("should output name and age", () => {
   const text = generateText("javan", 38);
@@ -14,7 +16,25 @@ test("should output data-less text", () => {
   expect(text).toBe(" (null years old)");
 });
 
-test('should generate a valid text output', () => {
-	const text = checkAndGenerate('javan', 38)
-	expect(text).toBe(javan)
-})
+test("should generate a valid text output", () => {
+  const text = checkAndGenerate("javan", 38);
+  expect(text).toBe(javan);
+});
+
+test("should create an element with text and correct class", async () => {
+  const browser = await puppeteer.launch({
+    // headless: false,
+    headless: true,
+    // slowMo: 80,
+    // args: ["--window-size=1920,1080"],
+  });
+  const page = await browser.newPage();
+  await page.goto("http://127.0.0.1:5500/index.html");
+  await page.click("input#name");
+  await page.click("input#name");
+  await page.type("input#name", 'Anna');
+  await page.type("input#age", '29');
+	await page.click('#btnAddUser')
+	const finalText = await page.$eval('.user-item', el => el.textContent)
+	expect(finalText).toBe(Anna)
+}, 10000);
